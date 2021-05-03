1c6f19abd1035e1eafd027461496bd67f432f68e
Fachrul Razi <fachrulzie@gmail.com>
2021-05-03 15:22:02 +0700

    Final Version, Checked and Run

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,68 @@
+import utils.SuitSystem
 import kotlin.system.exitProcess
 
 /*
+* ===== PSEUDOCODE =====
+* Buat Package Utils -> DONE
+* Buat Class Suit System -> DONE
+*   - Buat fun inputPlayer -> DONE
+*       - Input Player 1 -> DONE
+*       - Input Player 2 -> DONE
+*   - Buat funtion suitCondition -> DONE
+*   - Initialize fun suitCondition ke fun inputPlayer -> DONE
+* Bikin Class App -> DONE
+*   - Print Header -> DONE
+*   - Play Game -> DONE
+*   - Print Result -> DONE
+* Repeat Game? -> DONE
+* Check Every Result -> DONE (edit -> hapus fun suitCondition dan late init var karena hasil jadi draw terus)
+* Push Final Version to Git -> DONE
+* ===== PSEUDOCODE =====
+*/
+
+class App {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            printHeader()
+            SuitSystem().playerInput()
+            playAgain()
+        }
+
+        private fun printHeader() {
+            println("""
+        ==============================================
+        ========= GAME SUIT TERMINAL VERSION =========
+        ==============================================
+        """.trimIndent())
+        }
+
+        private fun playAgain() {
+            println("Main Lagi? (Y/N)")
+            if (readLine().equals("Y", ignoreCase = true)) {
+                println("Masukkan Jumlah Main Lagi : ")
+                val repeatPlay = readLine()?.toInt()
+                if (repeatPlay != null) {
+                    for (i in 1..repeatPlay) {
+                        println(" ")
+                        println("Permainan Ulang ke-$i")
+                        SuitSystem().playerInput()
+                    }
+                }
+            } else {
+                println("Terima Kasih Sudah Bermain")
+                exitProcess(0)
+            }
+        }
+    }
+}
+
+
+
+/* VERSI WORKING NON OOP ADA DI BAWAH, JANGAN DIHAPUS
+import kotlin.system.exitProcess
+
 * ===== PSEUDOCODE =====
 * Buat Package Utils ->
 * Buat Class Suit System ->
@@ -12,7 +74,10 @@ import kotlin.system.exitProcess
 * Print Result -> DONE
 * Repeat Game? -> DONE
 * ===== PSEUDOCODE =====
-*/
+
+problem :
+pake late init var ; var tsb harus diinialisasi, pas diisi "" ternyata hasilnya draw terus
+akhirnya dimasukkan ke val tanpa late init dan function suit system dihapus supaya bisa berjalan
 
 class App {
 
@@ -75,4 +140,5 @@ class App {
         }
     }
 }
-
+VERSI WORKING NON OOP, JANGAN DIHAPUS
+*/
